9f15960fca6f2243e083a6bb18500f7630a23779
Romaric Zounlome <rzounlom@gmail.com>
2023-10-20 08:46:31 -0400

    add new user functionality

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,33 @@
-export default function NewUser() {
-  <div>hi</div>;
+import { auth, currentUser } from "@clerk/nextjs";
+
+import { prisma } from "@/utils/db";
+import { redirect } from "next/navigation";
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  console.log(user);
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+
+  if (!match) {
+    //if user somehow got created without the db knowing, create a new user in the db
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect("/journal");
+};
+
+export default async function NewUser() {
+  await createNewUser();
+  return <div>...loading</div>;
 }
